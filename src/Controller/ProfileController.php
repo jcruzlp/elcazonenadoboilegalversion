404f9d90a30dec39149604b1f38018c1855c93d4
Salva Maine <salvador.maine@elca.ch>
2025-09-24 16:14:24 +0200

    feat: Add mock profile individual page.

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,17 +2,35 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Profile;
+use App\Repository\ProfileRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    #[Route('/profile', name: 'profiles')]
+    public function index(ProfileRepository $profileRepository): Response
     {
+        $profiles = $profileRepository->findAll();
+
         return $this->render('profile/index.html.twig', [
             'controller_name' => 'ProfileController',
+            'profiles' => $profiles,
         ]);
     }
+
+
+    #[Route('/profile/{id}', name: 'profile')]
+    public function profile(Profile $profile, ProfileRepository $profileRepository): Response
+    {
+        return $this->render('profile/show.html.twig', [
+            'controller_name' => 'ProfileController',
+            'profile' => $profile,
+        ]);
+    }
+
+
 }
